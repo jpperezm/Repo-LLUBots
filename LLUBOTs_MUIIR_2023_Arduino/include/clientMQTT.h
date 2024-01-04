6997110f932d71752453e248d8e7067c3238fab7
jpperezm <alu0101278919@ull.edu.es>
2024-01-04 17:48:27 +0000

    Update MQTT client configuration and topics

--- LLUBOTs_MUIIR_2023_Arduino/include/clientMQTT.h
+++ LLUBOTs_MUIIR_2023_Arduino/include/clientMQTT.h
@@ -4,9 +4,14 @@ extern WiFiClient wifiClient;
 // Initialize the MQTT client:
 extern PubSubClient MQTTClient;
 
+// LLUBot ID:
+extern int LLUBotID;
+
 // Initialize the MQTT client:
 extern const char *mqtt_broker;
 extern const char *movement_topic;
+extern const char *home_topic;
+extern const char *config_topic;
 const int mqtt_port = 1883;
 
 void mqttSetup();
--- LLUBOTs_MUIIR_2023_Arduino/src/clientMQTT.cpp
+++ LLUBOTs_MUIIR_2023_Arduino/src/clientMQTT.cpp
@@ -8,8 +8,14 @@ WiFiClient wifiClient;
 
 PubSubClient MQTTClient(wifiClient);
 
-const char *mqtt_broker = "192.168.84.74";
-const char *movement_topic = "LLUBot/movement";
+const char *mqtt_broker = "51.20.185.180";
+
+int LLUBotID;
+
+const char *movement_topic = "/LLUBot/movement/";
+const char *home_topic = "/LLUBot/NFC/"
+const char *config_topic = "/LLUBot/config";
+const char *mode_topic = "/LLUBot/mode/";
 
 // Initialize the Wi-Fi client
 void mqttSetup() {
@@ -20,39 +26,77 @@ void mqttSetup() {
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
-
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+  String message = convertPayloadToString(payload, length);
+  if (String(topic) == movement_topic) {
+    processMovementCommand(message);
+  }
 
+  if (String(topic) == home_topic) {
+    Serial.println("Going home");
+    //goHome();
+  }
+
+  if (String(topic) == mode_topic) {
+    Serial.println("Changing mode");
+    //changeMode(message);
+  }
+
+  if (String(topic) == config_topic) {
+    config(message);
+  }
   Serial.println();
-  Serial.println("-----------------------");
-  // Convert payload to String
+}
+
+String convertPayloadToString(byte *payload, unsigned int length) {
   String message;
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
   }
-  // Check if the message is for the movement topic
-  if (String(topic) == movement_topic) {
-    if (message == "forward") {
-      Serial.println("Moving forward");
-      //goStraight('F', 10);
-    } else if (message == "backward") {
-      Serial.println("Moving backward");
-      //goStraight('B', 10);
-    } else if (message == "left") {
-      Serial.println("Turning left");
-      //turn('L', 90);
-    } else if (message == "right") {
-      Serial.println("Turning right");
-      //turn('R', 90);
-    } else if (message == "stop") {
-      Serial.println("Stopping");
-      //stop();
+  return message;
+}
+
+void config(String message) {
+  if (message == "all") {
+    Serial.println("config all");
+    waitForID();
+  } else if (LLUBotID != NULL) {
+    if (message == LLUBotID) {
+      Serial.println("config " + String(LLUBotID));
+      waitForID();
     }
   }
-  Serial.println();
+}
+
+void processMovementCommand(const String &message) {
+  if (message == "forward") {
+    Serial.println("Moving forward");
+    //goStraight('F', 10);
+  } else if (message == "backward") {
+    Serial.println("Moving backward");
+    //goStraight('B', 10);
+  } else if (message == "left") {
+    Serial.println("Turning left");
+    //turn('L', 90);
+  } else if (message == "right") {
+    Serial.println("Turning right");
+    //turn('R', 90);
+  } else if (message == "stop") {
+    Serial.println("Stopping");
+    //stop();
+  }
+}
+
+void waitForID() {
+  int mockNFC = 1;
+  while (LLUBotID == NULL) {
+    Serial.println("Waiting for ID");
+    LLUBotID = mockNFC;
+    delay(1000);
+  }
 }
 
 void connect() {
@@ -61,7 +105,10 @@ void connect() {
 
     if (MQTTClient.connect("ESP8266Client")) {
       Serial.println("connected");
-      MQTTClient.subscribe(movement_topic);
+      MQTTClient.subscribe(movement_topic + LLUBotID);
+      MQTTClient.subscribe(home_topic + LLUBotID);
+      MQTTClient.subscribe(mode_topic + LLUBotID);
+      MQTTClient.subscribe(config_topic);
     } else {
       Serial.print("failed with state ");
       Serial.print(MQTTClient.state());
